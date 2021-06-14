6f9380abe87802afeabacd0fe4b83dfafe6c23c2
1chiSensei <mail@1chi.tk>
2021-06-14 10:48:15 +0800

    chore(setup): add new line

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -10,6 +10,7 @@ const db = prompt(chalk.yellowBright('What is the postgres db url?'));
 const env = stripIndents`
     DISCORD_TOKEN="${token}"
     DATABASE_URL="${db}"
+
 `;
 
 writeFileSync('./.env', env);
