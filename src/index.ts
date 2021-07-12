2f9faabf4301415c09e49023c177b586f24ca9e8
f1shy-dev <agarwal.vrishank@gmail.com>
2021-07-12 13:26:22 +0000

    fix: remove debug env logger

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import '@sapphire/plugin-logger/register';
 import '@skyra/editable-commands';
 import 'reflect-metadata';
 import 'dotenv/config';
-console.log(process.env);
 
 import { Client } from '@structures/Client';
 
